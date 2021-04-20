f7ac83960a6e7de4f6eb4eb8eec0663b06daf84d
GDaria-ent <red_potter@mail.ru>
2021-04-20 11:29:11 +0300

    nnn

--- scripts.js
+++ scripts.js
@@ -2,12 +2,14 @@ var checkbox = document.querySelector('.checkbox-switcher');
 var headAnimation = document.querySelector('.animation-wrapper');
 var pawAnimation = document.querySelector('.animation-paw');
 var speechAnimation= document.querySelector('.speechbubble');
+var mouthAnimation = document.querySelector('.mouth');
 checkbox.addEventListener('click',function(){
     checkbox.checked = true
     headAnimation.classList.add('animation-wrapper--visible');
+    mouthAnimation.classList.add('mouth--visible');
     speechAnimation.classList.add('speechbubble--visible');
     pawAnimation.classList.add('animation-paw--visible');
-    
+    setTimeout(removeClasses, 4000);
 } );
 
 // pawAnimation.addEventListener('animationend',function(){
@@ -23,4 +25,3 @@ function removeClasses(){
     speechAnimation.classList.remove('speechbubble--visible');
     pawAnimation.classList.remove('animation-paw--visible');
 };
-setTimeout(removeClasses, 4000);
